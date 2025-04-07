22df54d56535aba42190fbd90c87a1b7ebd9df6d
Eunmi Lee <eunoia0235@gmail.com>
2025-04-07 13:19:34 +0900

    feat: 리액트 앱 생성 및 jsx 문법 연습

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let name = '리액트';
+  const style = {
+    backgroundColor: 'black',
+    color: 'white',
+    fontSize: '48px',
+    fontWeight: 'bold',
+    padding: '20px',
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p style={style}>{name === '리액트' ? <h1>YES</h1> : null}</p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
