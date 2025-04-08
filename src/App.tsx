3713795a3ff465a38dc64c10180cffd7ad1b6c54
Eunmi Lee <eunoia0235@gmail.com>
2025-04-08 22:49:34 +0900

    feat: 할일, 시계 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoList from './Todolist';
+import Clock from './Clock';
 
 function App() {
-  let name = '리액트';
-  const style = {
-    backgroundColor: 'black',
-    color: 'white',
-    fontSize: '48px',
-    fontWeight: 'bold',
-    padding: '20px',
-  };
-
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p style={style}>{name === '리액트' ? <h1>YES</h1> : null}</p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <TodoList></TodoList>
+      <Clock></Clock>
     </div>
   );
 }
